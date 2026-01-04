8ec71c108f49e3ccd748dafa3967ba5bf905e026
sergazin <nadikz4444@gmial.com>
2026-01-05 00:23:34 +0500

    init commit

---
+++ src/test/java/org/example/ortaback/OrtaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ortaback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrtaBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
